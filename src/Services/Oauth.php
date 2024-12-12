329902181d949051f2c50e730b09b29228b41fb9
{alex.qiubo@qq.com} <alex.qiubo@qq.com>
2024-12-12 11:22:40 +0800

    fixbug: fix get token error: uri not define

--- src/Services/Oauth.php
+++ src/Services/Oauth.php
@@ -3,6 +3,8 @@
 namespace HyperfAlliance\Vip\Services;
 
 use HyperfAlliance\Vip\Caller;
+use HyperfAlliance\Vip\Exception\ResultErrorException;
+use Psr\Http\Message\ResponseInterface;
 
 /**
  * Oauth
@@ -25,5 +27,58 @@ class Oauth extends Caller
             "vip@" . $account,
             urlencode($this->app->config->get("notify_url")),
         );
+    }
+
+    /**
+     * @param $code
+     * @param $client_ip
+     *
+     * @return array|mixed
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function buildAccessToken($code, $client_ip)
+    {
+        $request = [
+            "code"              => $code,
+            "request_client_ip" => $client_ip,
+            "grant_type"        => "authorization_code",
+            "client_id"         => $this->app->config->get("app_key"),
+            "client_secret"     => $this->app->config->get("app_secret"),
+            "redirect_uri"      => $this->app->config->get("notify_url"),
+        ];
+
+        $resp = $this->fetch($this->app->config->get("oauth_url") . "/oauth2/token", "POST", [
+            "headers" => [
+                "Accept"       => "application/json",
+                "Content-Type" => "application/json",
+            ],
+            "query"   => $request,
+        ]);
+        if (!isset($resp['access_token'])) {
+            throw new ResultErrorException($resp["msg"]);
+        }
+        return [
+            "access_token"  => $resp["access_token"],
+            "refresh_token" => $resp["refresh_token"],
+            "expires_in"    => $resp["expires_in"] + time(),
+        ];
+    }
+
+    /**
+     * @param ResponseInterface $response
+     *
+     * @return \AlexQiu\Sdkit\Support\Collection|array|false|mixed|object|ResponseInterface|string|null
+     * @throws \AlexQiu\Sdkit\Exceptions\InvalidConfigException
+     */
+    protected function unwrapResponse(ResponseInterface $response)
+    {
+        $res = $this->castResponseToType(
+            $response,
+            $this->app->getContainer()->get("config")->get('http.response_type')
+        );
+        if (!isset($res['access_token'])) {
+            throw new ResultErrorException($res["msg"] ?? $res["code"], 77000);
+        }
+        return $res;
     }
 }
--- src/Services/Token.php
+++ src/Services/Token.php
@@ -18,41 +18,6 @@ class Token extends Caller
     public string $version = "1.0.0";
 
     /**
-     * @param $code
-     * @param $client_ip
-     *
-     * @return array|mixed
-     * @throws \GuzzleHttp\Exception\GuzzleException
-     */
-    public function buildAccessToken($code, $client_ip)
-    {
-        $request = [
-            "code"              => $code,
-            "request_client_ip" => $client_ip,
-            "grant_type"        => "authorization_code",
-            "client_id"         => $this->app->config->get("app_key"),
-            "client_secret"     => $this->app->config->get("app_secret"),
-            "redirect_uri"      => $this->app->config->get("notify_url"),
-        ];
-
-        $resp = $this->fetch($this->app->config->get("oauth_url"), "POST", [
-            "headers" => [
-                "Accept"       => "application/json",
-                "Content-Type" => "application/json",
-            ],
-            "query"   => $request,
-        ]);
-        if (!isset($resp['access_token'])) {
-            throw new ResultErrorException($resp["msg"]);
-        }
-        return [
-            "access_token"  => $resp["access_token"],
-            "refresh_token" => $resp["refresh_token"],
-            "expires_in"    => $resp["expires_in"] + time(),
-        ];
-    }
-
-    /**
      * @param $refresh_token
      * @param $client_ip
      *
